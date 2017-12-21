80dd1a44fed966e3659adce3c94fab7883be8c89
Alex Resiga <resiga_alex@yahoo.com>
2017-12-21 19:52:48 +0200

    added annotations div to the right side

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $.getJSON('https://6c05830f-a7ef-42b2-a90a-0b3c35cef64c.mock.pstmn.io/annotations', function (json) {
     for (var i = 0; i < json['categories'].length; ++i) {
         var category = json['categories'][i];
-        $('#annotations').append('<li>' + '<input type="checkbox">' + category['name'] + '<ul id="' + 'annotation' + i + '"></ul>' + '</li>');
+        $('#annotations').append('<div class="checkbox"><label><input type="checkbox" name="optionsCheckboxes"> <span class="checkbox-material"><span class="check"></span></span>' + category['name'] + '</input></label></div><div style="border-left: 45px solid #1F3249;" class="annotChild" id="' + 'annotation' + i + '"></div>');
 
         for (var j = 0; j < category['items'].length; ++j) {
             var item = category['items'][j];
-            $('#annotation' + i).append('<li>' + '<input type="checkbox">' + item + '</li>');
+            $('#annotation' + i).append('<div class="checkbox"><label><input type="checkbox" name="optionsCheckboxes"> <span class="checkbox-material"><span class="check"></span></span>' + item + '</label></div>');
         }
         $('#annotation' + i).append()
     }
