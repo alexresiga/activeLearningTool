cb56dae7ab65389f0532810b05830cf7cdf3abec
matt <stroiamatei@gmail.com>
2017-12-30 19:28:46 +0200

    jquery master

--- main.js
+++ main.js
@@ -10,12 +10,17 @@ $.getJSON('https://6c05830f-a7ef-42b2-a90a-0b3c35cef64c.mock.pstmn.io/annotation
     }
 });
 
-$.getJSON('https://raw.githubusercontent.com/alexresiga/activeLearningTool/master/valid_filtered_0236.json', function(json) {
+$.getJSON('https://raw.githubusercontent.com/alexresiga/activeLearningTool/master/valid_filtered_0236.json', function (json) {
     $('#title').html(json['metadata']['title']);
     $('#abstract').html(json['documentAbstract']);
     $('#content').html(json['sections'][0]['content']);
-    for ( var i = 0; i <json['metadata']['authors'].length; ++i) {
-        var fullname = json['metadata']['authors'][i]['givenName'] + ' ' +json['metadata']['authors'][i]['surName'];
-        $('#authors').append('<div id="author'+ i +'" class="author">'+ fullname +'</div>');
+    for (var i = 0; i < json['metadata']['authors'].length; ++i) {
+        var fullname = json['metadata']['authors'][i]['givenName'] + ' ' + json['metadata']['authors'][i]['surName'];
+        $('#authors').append('<div id="author' + i + '" class="author">' + fullname + '</div>');
     }
+});
+
+$('.dropdown-menu').children().on('click', function () {
+    $('#dropdown').find('a').first().text($(this).text());
+    $('#dropdown').find('a').first().append('<b class="caret"></b>');
 });
