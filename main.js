412a96d43f3d5bc8fc3bcc10a8778e69106f79f8
Alex Resiga <resiga_alex@yahoo.com>
2018-01-10 00:26:14 +0200

    fixed sizing issues

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $.getJSON('https://6c05830f-a7ef-42b2-a90a-0b3c35cef64c.mock.pstmn.io/annotations', function (json) {
     for (var i = 0; i < json['categories'].length; ++i) {
         var category = json['categories'][i];
-        $('#annotations').append('<div class="checkbox"><label><input type="checkbox" name="optionsCheckboxes"> <span class="checkbox-material"><span class="check"></span></span>' + category['name'] + '</input></label></div><div style="border-left: 45px solid #1F3249;" class="annotChild" id="' + 'annotation' + i + '"></div>');
+        $('#annotations').append('<div class="checkbox"><label style="color:white!important;font-size:15px;"><input type="checkbox" name="optionsCheckboxes" > <span class="checkbox-material"><span class="check" style="width:15px;height:15px;"></span></span>' + category['name'] + '</input></label></div><div style="border-left: 45px solid #1F3249;" class="annotChild" id="' + 'annotation' + i + '"></div>');
 
         for (var j = 0; j < category['items'].length; ++j) {
             var item = category['items'][j];
-            $('#annotation' + i).append('<div class="checkbox"><label><input type="checkbox" name="optionsCheckboxes"> <span class="checkbox-material"><span class="check"></span></span>' + item + '</label></div>');
+            $('#annotation' + i).append('<div class="checkbox" ><label style="color:white!important;font-size:14px;"><input type="checkbox" name="optionsCheckboxes"> <span class="checkbox-material"><span class="check" style="width:14px;height:14px;"></span></span>' + item + '</label></div>');
         }
     }
 });
